3c12060959116a9b38ba9622202f7c42a9ba9d8f
Michael Langley <michaellangley@disroot.org>
2022-06-12 16:29:59 +0200

    Accidentally didn't take out the test data

--- notification-handler/notification.py
+++ notification-handler/notification.py
@@ -3,6 +3,7 @@ import os
 import desktop_notifier
 import asyncio
 import requests
+import argparse
 
 def download_from_transfersh(message: str) -> None:
     file_request = requests.get(message)
@@ -40,11 +41,23 @@ async def send_notification(title: str, message: str, app_name: str) -> None:
         buttons=buttons
     )
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='A utility to handle notifications send from the send_to_phone '
+                                                 'Python script')
+    parser.add_argument('message', help='Body of the notification')
+    parser.add_argument('-t', '--title', help='Title of the notification', default='No title')
+    parser.add_argument('--app-name', help='App name of the notification', default='Python app')
+    return parser.parse_args()
+
+async def wait(seconds: int):
+    await asyncio.sleep(seconds)
 
 if __name__ == "__main__":
+    args = parse_args()
     loop = asyncio.get_event_loop()
-    loop.create_task(send_notification('Title', 'https://transfer.sh/PDQQwl/Send%20to%20phone.md', 'Test App'))
-    loop.run_forever()
+    loop.create_task(send_notification(args.title, args.message, args.app_name))
+    loop.run_until_complete(wait(10))
+    # todo: figure out how to make a daemon that can respond to the notifications and not close after a while
 
 
 
